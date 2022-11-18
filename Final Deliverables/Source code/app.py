d4a47536696dee44b5a362afb6c5611247b2932f
Hariram B <93965734+Hariram2001@users.noreply.github.com>
2022-11-18 10:59:42 +0530

    Update app.py

--- Final Deliverables/Source code/app.py
+++ Final Deliverables/Source code/app.py
@@ -2,6 +2,12 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def bank():
-    return render_template('Chatbot.html')
+    return render_template('Home.html')
+@app.route('/about')
+def about():
+    return render_template('About.html')
+@app.route('/contact')                           
+def contact():
+    return render_template('Contact.html')
 if __name__ =='__main__':
     app.run(debug = True)
